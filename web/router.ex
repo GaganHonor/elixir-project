7b4dbf992eab293b5fb0728067a9fd53e412934b
Gagan <141204441+GaganHonor@users.noreply.github.com>
2023-12-16 11:06:03 +0530

    Update router.ex

--- web/router.ex
+++ web/router.ex
@@ -1,1 +1,11 @@
+defmodule TimeZoneWeb.Router do
+  use Phoenix.Router
 
+  pipeline :browser do
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_csrf
+  end
+
+  get "/", to: TimeZoneController, :index
+end
